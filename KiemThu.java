adc24200914c8396c93e81f85ac36ae1b4d5af98
21010647AnNV <95534256+21010647AnNV@users.noreply.github.com>
2024-04-19 10:00:17 +0700

    Update kiemthu

--- KiemThu.java
+++ KiemThu.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class KiemThu {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int choice;
